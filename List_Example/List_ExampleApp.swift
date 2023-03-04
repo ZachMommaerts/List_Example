d4242f3bfe59723e897bdf03dd9f55bb805f5da2
ZachMommaerts <zmommaerts@gmail.com>
2023-03-03 22:26:50 -0600

    Initial Commit

---
+++ List_Example/List_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_ExampleApp.swift
+//  List_Example
+//
+//  Created by Zach Mommaerts on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct List_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
